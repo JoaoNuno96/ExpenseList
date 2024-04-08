aebe1c425185aceb8b28ae6a9459d2a61a708f46
JoaoNuno96 <joaocouto96@hotmail.com>
2024-04-08 15:03:59 +0100

    Simple Application

---
+++ ExpensesList/Entities/Expense.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesList.Entities
+{
+    class Expense
+    {
+        public string Name { get; set; }
+        public double Price { get; private set; }
+        public DateTime Date { get; set; }
+
+        public Expense() { }
+
+        public Expense(string n, double price)
+        {
+            Nome = n;
+            Price = price;
+            Date = DateTime.Now;
+        }
+
+        public string Nome
+        {
+            set
+            {
+                if (value.Length > 3 && value.Length < 20)
+                {
+                    Name = value;
+                }
+                else
+                {
+                    Name = "";
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Name}, Preço: {Price}, Date: {Date.ToShortDateString()}";
+        }
+
+
+    }
+}
---
+++ ExpensesList/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ExpensesList.Entities;
+
+namespace ExpensesList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("______________________________EXPENSES LIST_________________________________");
+            Console.WriteLine("This is a simple program, which lets you save all your expenses into a list.");
+            Console.WriteLine("");
+            Console.WriteLine("____________________________________________________________________________");
+
+            bool execute = true;
+            List<Expense> ExList = new List<Expense>();
+
+            while (execute)
+            {
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("Do you want to add an expense?(e) ");
+                Console.WriteLine("Do you want to show current list ?(l) ");
+                Console.WriteLine("Turn Off Application? (s) ");
+
+                char Choice = char.Parse(Console.ReadLine());
+
+
+                if (Choice == 's' || Choice == 'S')
+                {
+                    execute = false;
+
+                }
+
+                if (Choice == 'e' || Choice == 'E')
+                {
+                    Console.WriteLine("");
+                    Console.Write("How many expenses will you add? ");
+                    int Nr = int.Parse(Console.ReadLine());
+
+                    for (int i = 1; i <= Nr; i++)
+                    {
+                        Console.WriteLine("____________________________________________________________________________");
+                        Console.WriteLine("Expense #" + i);
+                        Console.Write("Name: ");
+                        string Name = Console.ReadLine();
+                        Console.Write("Price: ");
+                        double Price = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                        ExList.Add(new Expense(Name,Price));
+                        Console.WriteLine("Expense add with success!");
+                        Console.WriteLine("____________________________________________________________________________");
+
+                    }
+                }
+
+                if(Choice == 'l' || Choice == 'L')
+                {
+                    Console.WriteLine("");
+                    double Gastos = 0;
+
+                    if(!ExList.Any())
+                    {
+                        Console.WriteLine("____________________________________________________________________________");
+                        Console.WriteLine("No expense registered.");
+                        Console.WriteLine("____________________________________________________________________________");
+                    }
+                    else
+                    {
+                        Console.WriteLine("____________________________________________________________________________");
+                        Console.WriteLine("LIST OF EXPENSES:");
+                        foreach (Expense ex in ExList)
+                        {
+                            
+                            Console.Write(" ----> ");
+                            Console.WriteLine(ex.ToString());
+                            Gastos += ex.Price;
+
+                        }
+
+                        Console.WriteLine("");
+                        Console.WriteLine($"Total Spend: {ExList.Select(x => x.Price).Sum()}$");
+                        Console.WriteLine("____________________________________________________________________________");
+                    }
+                    Console.WriteLine("");
+
+                }
+
+            }
+            Console.WriteLine("____________________________________________________________________________");
+            Console.WriteLine("Application Shut Down!");
+            Console.WriteLine("____________________________________________________________________________");
+
+        }
+    }
+
+
+}
